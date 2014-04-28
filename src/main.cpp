9c008feeb9a313b16d67c7b8e303de3d17910e66
element94project <element94@gmail.com>
2014-04-28 03:37:24 -0500

    tm

--- src/main.cpp
+++ src/main.cpp
@@ -2518,10 +2518,10 @@ bool LoadBlockIndex(bool fAllowNew)
 
         CBlock block;
         block.vtx.push_back(txNew);
-        block.hashPrevBlock = 00;
-        block.hashMerkleRoot = block.BuildMerkleTree(2072176646);
+        block.hashPrevBlock = 0;
+        block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1398592918;
+        block.nTime    = 1398674136;
         block.nBits    = bnProofOfWorkLimit.GetCompact();
         block.nNonce   = 237454;
 
@@ -2535,7 +2535,7 @@ bool LoadBlockIndex(bool fAllowNew)
 
 
         assert(block.hashMerkleRoot == uint256("0x207217664685dff6697cbfb19cc1fc1943dbbcad1d1c1ab6072a9c997e0be693"));
-		assert(block.GetHash(46) == (!fTestNet ? hashGenesisBlock : ));
+		assert(block.GetHash() == (!fTestNet ? hashGenesisBlock : ));
 
         // Start new block file
         unsigned int nFile;
